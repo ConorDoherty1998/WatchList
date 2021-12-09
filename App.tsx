23a20abda066a16330ae0a328113a5c21bb581ff
ConorDoherty1998 <Conordoherty980@gmail.com>
2021-12-09 18:50:14 +0000

    Boiler plate folder structure

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Auth } from './src/app'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Auth />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: 40,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
---
+++ src/app/containers/auth/main/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+type AuthProps = {
+
+}
+
+const Auth: React.FC<AuthProps> = () => {
+    return (
+        <View>
+            <Text>Auth Page Here</Text>
+        </View>
+    )
+}
+
+
+export { Auth };
